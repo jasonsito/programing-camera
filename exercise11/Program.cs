09cd053c8f0b23df4511de3b670e3ad214438b26
jasonsito <alumno@viralgamingacademy.com>
2020-02-20 13:30:49 +0100

    ejercicios de clase

---
+++ exercise11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercise11
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Indica cuantos numeros quieres sumar");
+      int numbercount = int.Parse(Console.ReadLine());
+
+      int[] numbers = new int[numbercount];
+      for (int i = 0; i < numbers.Length; i++)
+      {
+        Console.Write("Número " + (i + 1) + ": ");
+        numbers[i] = int.Parse(Console.ReadLine());
+      }
+      int total = 0;
+      for (int i = 0; i < numbers.Length; i++)
+      {
+        total += numbers[i];
+
+
+      }
+      Console.WriteLine("Suma total: " + total);
+
+    }
+  }
+}
